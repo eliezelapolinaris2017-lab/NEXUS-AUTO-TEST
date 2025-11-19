0c530f1bc8b3655f2fcdc3fca2ec772d39d31b25
Facturas <eliezelapolinaris2017@gmail.com>
2025-11-19 08:11:37 -0400

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
   projectId: "nexus-auto-pro-2026",
   storageBucket: "nexus-auto-pro-2026.firebasestorage.app",
   messagingSenderId: "308014641424",
-  appId: "1:308014641424:web:5157d1267e280c48eeb595"
+  appId: 
 };
 
 firebase.initializeApp(firebaseConfig);
